fc15f29e1481d32094c3bfed1702684e3a8b3902
Norman Steger <normansteger@gmail.com>
2017-10-27 22:09:42 +0200

    add node_env production to webpack prod.config.js

--- config/webpack/prod.config.js
+++ config/webpack/prod.config.js
@@ -5,6 +5,9 @@ const baseConfig = require('./base.config.js')
 module.exports = merge(baseConfig, {
   // prod config here
   plugins: [
+    new webpack.EnvironmentPlugin({
+       NODE_ENV: 'production'
+    })
     new webpack.optimize.UglifyJsPlugin({
       sourceMap: true,
       compressor: {
